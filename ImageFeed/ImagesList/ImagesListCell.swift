92987a9c45a1832fe77963b092b91244b180df51
Balamut <Balamut878S@ya.ru>
2024-11-24 20:00:51 +0300

    Сделал градиент

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -13,4 +13,25 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var imageCell: UIImageView!
     @IBOutlet var likeButton: UIButton!
     @IBOutlet var dataLabel: UILabel!
+    private let gradientLayer = CAGradientLayer()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupGradientForLabel()
+    }
+
+    private func setupGradientForLabel() {
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.6).cgColor
+        ]
+        gradientLayer.locations = [0.0, 1.0]
+        dataLabel.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = dataLabel.bounds
+    }
 }
+
