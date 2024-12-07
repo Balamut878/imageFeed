ae870276b9621de1790e17e8bb0d3cc3e1d0a010
Balamut <Balamut878S@ya.ru>
2024-12-07 12:48:04 +0300

    Добавлена навигация по проекту

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Дудченко on 26.11.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private weak var avatarImageView: UIImageView!
+    @IBOutlet private weak var logoutButton: UIButton!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton() {
+        
+    }
+}
